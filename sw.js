a2eef8c713176444dcdcf5b72ad07699b7429b58
kvschin <kvschin@gmail.com>
2026-09-24 14:07:43 -0500

    Release toolbar refresh as 0.9.22

--- sw.js
+++ sw.js
@@ -39,7 +39,7 @@
    subpath — GitHub Pages serves this from /PerenialDesignKK/. */
 'use strict';
 
-const VERSION = '0.9.21';
+const VERSION = '0.9.22';
 const CACHE = 'pocket-prairie-v' + VERSION;
 
 const PRECACHE = [
